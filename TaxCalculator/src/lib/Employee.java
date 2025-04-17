63dad7835e5d5ef13b44f7406887dd856e5ac7f3
kyntar <kyntarlubis21@gmail.com>
2025-04-17 07:17:14 +0700

    Memperbaiki Magic Numbers & Code Duplication
    Angka gaji (3_000_000, 5_000_000, 7_000_000) dan pengali 1.5 dipakai langsung dan duplikasi logika WNA salah

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -1,7 +1,6 @@
 package lib;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -58,24 +57,19 @@ public class Employee {
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
 	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}
-	}
+	 public void setMonthlySalary(int grade) {
+        int base;
+        switch (grade) {
+            case 1 -> base = 3_000_000;
+            case 2 -> base = 5_000_000;
+            case 3 -> base = 7_000_000;
+            default -> throw new IllegalArgumentException("Invalid grade: " + grade);
+        }
+        if (isForeigner) {
+            base = (int)(base * 1.5);
+        }
+        this.monthlySalary = base;
+    }
 	
 	public void setAnnualDeductible(int deductible) {	
 		this.annualDeductible = deductible;
